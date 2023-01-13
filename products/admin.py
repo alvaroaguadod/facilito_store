5fabceeb2be036fab9a77bf578ef382819560f8d
Álvaro Aguado <65191952+alvaroaguadod@users.noreply.github.com>
2023-01-13 16:29:58 +0100

    13/01

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'price')
+    list_display = ('_str_', 'slug', 'created_at')
+
+admin.site.register(Product, ProductAdmin)
